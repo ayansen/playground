120422e15c73b04dfae00ea905b234357cc7547e
Ayan Sen <ayansen89@gmail.com>
2022-08-15 22:45:49 -0700

    added hello world graphql query

--- graphql/src/main/kotlin/ayansen/playground/graphql/server/App.kt
+++ graphql/src/main/kotlin/ayansen/playground/graphql/server/App.kt
@@ -1,4 +1,4 @@
-package ayansen.playground.graphql
+package ayansen.playground.graphql.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
