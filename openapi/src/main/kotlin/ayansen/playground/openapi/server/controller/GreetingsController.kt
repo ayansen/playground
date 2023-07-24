24787b2f46a438a9d2c6f892193bc1b1963beb24
Ayan Sen <ayansen89@gmail.com>
2023-07-24 15:35:21 -0700

    moved common springboot dependencies to gradle plugin (#10)
    * moved common springboot dependencies to gradle plugin
    
    Signed-off-by: Ayan Sen <ayansen89@gmail.com>
    
    * updated kotlin version
    
    Signed-off-by: Ayan Sen <ayansen89@gmail.com>
    
    ---------
    
    Signed-off-by: Ayan Sen <ayansen89@gmail.com>

--- openapi/src/main/kotlin/ayansen/playground/openapi/server/controller/GreetingsController.kt
+++ openapi/src/main/kotlin/ayansen/playground/openapi/server/controller/GreetingsController.kt
@@ -1,6 +1,7 @@
 package ayansen.playground.openapi.server.controller
 
 import ayansen.playground.openapi.server.service.GreetingsService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/greetings")
 class GreetingsController(private val greetingsService: GreetingsService) {
 
-    @GetMapping("/{id}")
-    suspend fun getEmployeeById(@PathVariable name: String): String {
+    @Operation(summary = "Greet by name", description = "Returns a greetings message with your name")
+    @GetMapping("/{name}")
+    suspend fun greet(@PathVariable name: String): String {
         return greetingsService.greetPerson(name)
     }
 }
