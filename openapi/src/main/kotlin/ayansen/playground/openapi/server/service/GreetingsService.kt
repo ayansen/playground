8217eca7c30f527c8025b55a0eb896a91858b93f
Ayan Sen <ayansen89@gmail.com>
2023-07-24 15:31:39 -0700

    updated kotlin version
    Signed-off-by: Ayan Sen <ayansen89@gmail.com>

--- openapi/src/main/kotlin/ayansen/playground/openapi/server/service/GreetingsService.kt
+++ openapi/src/main/kotlin/ayansen/playground/openapi/server/service/GreetingsService.kt
@@ -1,12 +1,8 @@
 package ayansen.playground.openapi.server.service
 
-import io.swagger.v3.oas.annotations.Operation
-import org.springframework.web.bind.annotation.GetMapping
-
 class GreetingsService {
 
-    @Operation(summary = "Greet by name", description = "Returns a greetings message with your name")
-    @GetMapping("/{name}")
+
     fun greetPerson(name:String) : String {
         return "Hello $name"
     }
