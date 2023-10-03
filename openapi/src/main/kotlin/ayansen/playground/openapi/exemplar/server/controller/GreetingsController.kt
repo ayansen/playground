8fa3f72ae3e59435d24540672a6f4f0d490d02d6
Ayan Sen <ayansen89@gmail.com>
2023-10-02 20:47:58 -0700

    merged master
    Signed-off-by: Ayan Sen <ayansen89@gmail.com>

--- openapi/src/main/kotlin/ayansen/playground/openapi/exemplar/server/controller/GreetingsController.kt
+++ openapi/src/main/kotlin/ayansen/playground/openapi/exemplar/server/controller/GreetingsController.kt
@@ -1,6 +1,7 @@
 package ayansen.playground.openapi.exemplar.server.controller
 
 import ayansen.playground.openapi.server.service.GreetingsService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
